2d962e1c8ea9a254ea8a6d2a5620de850d3a9e83
haxwithaxe <spam@haxwithaxe.net>
2026-03-11 18:01:06 -0400

    Added pre and post build script runners to build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
+set -e
+
 if [ "$(whoami)" != "root" ]; then
 	echo "This must be run as root"
 	exit 1
 fi
 
-set -ex
+if compgen -G 'build.sh.d/*.pre'; then
+	echo Running pre-build scripts
+	set -x
+	for script in build.sh.d/*.pre; do
+		bash $script
+	done
+	set +x
+fi
+
+set -x
 
 lb clean
 lb config --debian-installer live
 lb build
 
+set +x
+
+if compgen -G 'build.sh.d/*.post'; then
+	echo Running post-build scripts
+	set -x
+	for script in build.sh.d/*.post; do
+		bash $script
+	done
+	set +x
+fi
 # FIXME: Add upload step?
 
